c0cca0be4d1cd2ba81c5c49e5d3a5e157c3d650f
Abhisek Daude <abhisekdaude1@gmail.com>
2025-01-23 02:55:49 +0545

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const userModel = require("./models/user");
 
 app.set ("view engine", "ejs");
 app.use(express.json());
@@ -16,8 +17,18 @@ app.get('/read' ,(req, res)=>{
     res.render("read");
 })
 
-app.post('/create', (req,res)=>{
-    
+app.post('/create', async (req,res)=>{
+
+   let {name,email,image}=req.body; // this code is used to put the name email and image enter by the user in body of the web page 
+
+   let createdUser= await userModel.create({  // we create the user 
+    name:name,
+    email:email,
+    image:image
+   });
+
+   res.send(createdUser);
+
 })
 
 app.listen(3000,()=>{
